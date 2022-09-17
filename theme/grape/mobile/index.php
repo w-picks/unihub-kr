cbbcd45187257bf9bff6931af5856881b8c3f15c
eden <goodeden3@gmail.com>
2022-09-17 17:18:40 +0900

    :zap: 최신 보도자료 메인 노출

--- theme/grape/mobile/index.php
+++ theme/grape/mobile/index.php
@@ -75,6 +75,14 @@ include_once(G5_THEME_MOBILE_PATH.'/head.php');
     </section>
 </div>
 
+<!-- 보도자료 -->
+<?php
+// 이 함수가 바로 최신글을 추출하는 역할을 합니다.
+// 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
+// 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
+echo latest('theme/review', 'broadcast', 5, 20);
+?>
+
 <script>
     const swiper = new Swiper('.project_slide_content', {});
 </script>
