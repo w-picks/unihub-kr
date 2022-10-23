c683ce7201cd165830ad9c3e5198e0e8f9f21ef6
eden <goodeden3@gmail.com>
2022-10-23 20:22:13 +0900

    :zap: 보도자료 메인 출력

--- theme/grape/mobile/index.php
+++ theme/grape/mobile/index.php
@@ -140,7 +140,7 @@ include_once(G5_THEME_MOBILE_PATH.'/head.php');
 // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
 // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
 // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-// echo latest('theme/review', 'broadcast', 5, 20);
+echo latest('theme/review', 'broadcast', 3, 30);
 ?>
 
 <!-- <section id="section7" class="kakao_plus_banner">
