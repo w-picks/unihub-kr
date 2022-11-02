401cc2e536a0c6b719d0914b6e41aa47c09d9693
eden <goodeden3@gmail.com>
2022-11-02 14:37:25 +0900

    :zap: 주소 파라미터 추가 2

--- theme/grape/mobile/skin/outlogin/basic/outlogin.skin.1.php
+++ theme/grape/mobile/skin/outlogin/basic/outlogin.skin.1.php
@@ -7,10 +7,15 @@ add_stylesheet('<link rel="stylesheet" href="'.$outlogin_skin_url.'/style.css">'
 <div class="ol">
   <a href="<?php echo G5_BBS_URL ?>/login.php" class="btn_s" style="padding-right: 10px;">로그인</a>
   <a href="<?php echo G5_BBS_URL ?>/register.php" class="btn_s " style="padding-left: 10px;">회원가입</a>
-  <a href="<?php echo G5_BBS_URL ?>/qawrite.php" class="btn_s contact_us" style="padding-left: 10px;">문의하기</a>
+  <a href="<?php echo G5_BBS_URL ?>/faq.php?la=kr" class="btn_s contact_us" style="padding-left: 10px;" onclick="contactUs()">문의하기</a>
   <a href="<?php echo G5_URL ?>" style="border-right:1px solid #8496ba; height:10px; line-height:10px;">kr</a>
-    <a href="<?php echo G5_URL ?>/eng" style="height:10px; line-height:10px;">en</a>
+  <a href="<?php echo G5_URL ?>/eng" style="height:10px; line-height:10px;">en</a>
 </div>
 
 <script>
+  const contactUs = () => {
+    // window.location.href='<?php echo G5_BBS_URL ?>/qawrite.php'
+    // window.location.href='<?php echo G5_BBS_URL ?>/faq.php?la=kr'
+    localStorage.setItem("lang", "kr")
+  };
 </script>
